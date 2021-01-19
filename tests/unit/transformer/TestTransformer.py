1c16ca7a636bc417e8f16cb4cf9371f3f4686ac4
vak <vak@macbookpro.local>
2021-01-19 15:59:49 +0100

    First try TDD

--- tests/unit/transformer/TestTransformer.py
+++ tests/unit/transformer/TestTransformer.py
@@ -1,5 +1,6 @@
 import unittest
 import transformer
+from tests.unit.transformer import TransformerFixtures as fixtures
 
 
 class TestTransformer(unittest.TestCase):
@@ -7,6 +8,9 @@ class TestTransformer(unittest.TestCase):
     def test_transform(self):
         self.assertEqual(4, transformer.Transformer.transform(2))
 
+    def test_unit_type(self):
+        actual = transformer.Transformer.transform_unit_type(fixtures.unit_type)
+        self.assertEqual(fixtures.expected_unit_type, actual)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/unit/transformer/TransformerFixtures.py
@@ -0,0 +1,29 @@
+unit_type = {
+    "name": "PERSON",
+    "title": [
+        {
+            "languageCode": "no",
+            "value": "Person"
+        },
+        {
+            "languageCode": "en",
+            "value": "Person"
+        }
+    ],
+    "description": [
+        {
+            "languageCode": "no",
+            "value": "Statistisk enhet er person (individ, enkeltmenneske)."
+        },
+        {
+            "languageCode": "en",
+            "value": "Statistical unit is person"
+        }
+    ]
+}
+
+expected_unit_type = {
+    'name': 'PERSON',
+    'label': 'Person',
+    'description': 'Statistisk enhet er person (individ, enkeltmenneske).'
+}
