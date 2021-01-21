48127d42de91ef6debf3c32398e2728396aca0f0
pawbu <pawbu@users.noreply.github.com>
2021-01-21 10:28:13 +0100

    Be able to run test from command line

--- tests/unit/transformer/test_transformer.py
+++ tests/unit/transformer/test_transformer.py
@@ -1,11 +1,19 @@
+import os.path
+import sys
+
+if __name__ == '__main__':
+    # Only munge path if invoked as a script. Testrunners should have setup
+    # the paths already
+    print("test_transformer.py __main__ called")
+    sys.path.insert(0, os.path.abspath(os.path.join(os.pardir, os.pardir, os.pardir)))
+
 import unittest
-import transformer
+from transformer import Transformer
 from tests.unit.transformer import TransformerFixtures as fixtures
 
 
 class TestTransformer(unittest.TestCase):
-
-    t = transformer.Transformer()
+    t = Transformer()
 
     def test_name_title_description(self):
         """ Common test for UnitType and SubjectField """
@@ -14,4 +22,7 @@ class TestTransformer(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    print("Paths used:")
+    for place in sys.path:
+        print(place)
     unittest.main()
