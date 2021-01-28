4fceefec731166a21e7b7063e3f881b9ce5b6d55
pawbu <pawbu@users.noreply.github.com>
2021-01-28 16:06:12 +0100

    Add method for updating metadata_all file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+
 import transformer as t
 
-#t.transform_to_file()
+# t.transform_to_file()
 
-#t.update_metadata_all_file(None, None)
+t.update_metadata_all_file(
+    {'name': 'TEST', 'title': 'Some title'},
+    Path('tests/resources/TestDataStore/no_ssb_test/metadata/metadata-all__1_0_0.json')
+)
--- transformer/__init__.py
+++ transformer/__init__.py
@@ -1,16 +1,24 @@
 import json
 from pathlib import Path
 
-from .Transformer import Transformer
+from .transformer import Transformer
 
 
 def transform_to_file():
     pass
 
 
-def update_metadata_all_file(source_path: Path, dataset: dict) -> None:
+def update_metadata_all_file(dataset: dict, source_path: Path) -> None:
     metadata_all_json = json.loads(source_path.read_text())
 
-    for data_structure in metadata_all_json['dataStructures']:
+    found_existing = False
+    for i, data_structure in enumerate(metadata_all_json['dataStructures']):
         if data_structure['name'] == dataset['name']:
-            return True
+            metadata_all_json['dataStructures'][i] = dataset
+            found_existing = True
+
+    if not found_existing:
+        metadata_all_json['dataStructures'].append(dataset)
+
+    source_path.write_text(json.dumps(metadata_all_json, sort_keys=False, indent=4, ensure_ascii=False))
+
