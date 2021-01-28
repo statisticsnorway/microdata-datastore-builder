7de3385a3a5164cb53b5d6948ed6c16d0890b4ff
pawbu <pawbu@users.noreply.github.com>
2021-01-28 15:09:03 +0100

    Add structure for updating metadata_all file

---
+++ main.py
@@ -0,0 +1,5 @@
+import transformer as t
+
+#t.transform_to_file()
+
+#t.update_metadata_all_file(None, None)
--- transformer/__init__.py
+++ transformer/__init__.py
@@ -1,1 +1,16 @@
+import json
+from pathlib import Path
+
 from .Transformer import Transformer
+
+
+def transform_to_file():
+    pass
+
+
+def update_metadata_all_file(source_path: Path, dataset: dict) -> None:
+    metadata_all_json = json.loads(source_path.read_text())
+
+    for data_structure in metadata_all_json['dataStructures']:
+        if data_structure['name'] == dataset['name']:
+            return True
