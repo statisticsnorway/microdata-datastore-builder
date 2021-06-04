99c4cb0f06821479026507b652fc2443027e1213
vak <vak@macbookpro.local>
2021-06-04 15:35:11 +0200

    Try pathlib

--- tests/unit/transformer/test_transformer_V2.py
+++ tests/unit/transformer/test_transformer_V2.py
@@ -1,19 +1,55 @@
-import os
+import os.path
+import sys
 import unittest
 import json
 
+import pathlib
+
 from transformer.transformer_V2 import Transformer
+from pathlib import Path
+
+if __name__ == '__main__':
+    # Only munge path if invoked as a script. Testrunners should have setup
+    # the paths already
+    print("test_transformer.py __main__ called")
+    sys.path.insert(0, os.path.abspath(os.path.join(os.pardir, os.pardir, os.pardir)))
 
 
 class TestTransformer(unittest.TestCase):
-    def setUp(self):
-        self.transformer = Transformer()
+
+    # def setUp(self):
+    #     self.transformer = Transformer()
+
+    # transformer = None
+    # json_file = None
+    # json_file_expected = None
+    #
+    # @classmethod
+    # def setUpClass(cls):
+    #     cls.transformer = Transformer()
+    #     cls.json_file = "tests/resources/transformer/KREFTREG_DS_enumerated.json"
+    #     cls.json_file_expected = "tests/resources/transformer/KREFTREG_DS_enumerated_expected.json"
+
+    transformer = Transformer()
+
+    def test_paths(self):
+        print (Path.home())
+        print (Path.cwd())
+        print(Path.cwd().parent)
+        print(Path.cwd().parent.parent)
+        print (Path('../../resources/transformer/KREFTREG_DS_described.json').resolve())
+        print (Path.cwd().parent.parent.joinpath('resources', 'transformer', 'KREFTREG_DS_described.json'))
 
     def test_dataset_with_enumerated_valuedomain(self):
-        with open(os.path.abspath("../../resources/transformer/KREFTREG_DS_enumerated.json")) as json_file:
+        json_path = Path('../../resources/transformer/KREFTREG_DS_described.json')
+        expected_json_path = Path('../../resources/transformer/KREFTREG_DS_described_expected.json')
+
+        with open(json_path.resolve()) as json_file:
             dataset = json.load(json_file)
-        with open(os.path.abspath("../../resources/transformer/KREFTREG_DS_enumerated_expected.json")) as expected_json_file:
+
+        with open(expected_json_path.resolve()) as expected_json_file:
             expected = json.load(expected_json_file)
+
         actual = self.transformer.transform_dataset(dataset)
         print(json.dumps(actual, indent=1))
         self.assertEqual(expected, actual)
