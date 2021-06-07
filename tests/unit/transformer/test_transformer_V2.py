ea4edc3978a5045d1734f707b3d9162adcfda895
vak <vak@macbookpro.local>
2021-06-07 11:40:29 +0200

    Clean up

--- tests/unit/transformer/test_transformer_V2.py
+++ tests/unit/transformer/test_transformer_V2.py
@@ -3,8 +3,6 @@ import sys
 import unittest
 import json
 
-#import pathlib
-
 from transformer.transformer_V2 import Transformer
 from pathlib import Path
 
@@ -19,15 +17,6 @@ class TestTransformer(unittest.TestCase):
 
     transformer = Transformer()
 
-    # def test_paths(self):
-    #     print (Path.home())
-    #     print (Path.cwd())
-    #     print (Path(__file__))
-    #     print(Path.cwd().parent)
-    #     print(Path(__file__).parent.parent)
-    #     print (Path('../../resources/transformer/KREFTREG_DS_described.json').resolve())
-    #     print (Path(__file__).parent.parent.parent.joinpath('resources', 'transformer', 'KREFTREG_DS_described.json'))
-
     def test_dataset_with_enumerated_valuedomain(self):
         dataset, expected = self.load_json_files('KREFTREG_DS_enumerated.json', 'KREFTREG_DS_enumerated_expected.json')
         actual = self.transformer.transform_dataset(dataset)
@@ -35,15 +24,6 @@ class TestTransformer(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_dataset_with_described_valuedomain(self):
-        # json_path = Path(__file__).parent.parent.parent.joinpath('resources', 'transformer', 'KREFTREG_DS_described.json')
-        # expected_json_path = Path(__file__).parent.parent.parent.joinpath('resources', 'transformer', 'KREFTREG_DS_described_expected.json')
-        #
-        # with open(json_path.resolve()) as json_file:
-        #     dataset = json.load(json_file)
-        #
-        # with open(expected_json_path.resolve()) as expected_json_file:
-        #     expected = json.load(expected_json_file)
-
         dataset, expected = self.load_json_files('KREFTREG_DS_described.json', 'KREFTREG_DS_described_expected.json')
         actual = self.transformer.transform_dataset(dataset)
         print(json.dumps(actual, indent=1))
@@ -51,15 +31,6 @@ class TestTransformer(unittest.TestCase):
 
     def test_dataset_with_no_attributes(self):
         dataset, expected = self.load_json_files('KREFTREG_DS_no_attributes.json', 'KREFTREG_DS_no_attributes_expected.json')
-        # json_path = Path(__file__).parent.parent.parent.joinpath('resources', 'transformer', 'KREFTREG_DS_no_attributes.json')
-        # expected_json_path = Path(__file__).parent.parent.parent.joinpath('resources', 'transformer', 'KREFTREG_DS_no_attributes_expected.json')
-        #
-        # with open(json_path.resolve()) as json_file:
-        #     dataset = json.load(json_file)
-        #
-        # with open(expected_json_path.resolve()) as expected_json_file:
-        #     expected = json.load(expected_json_file)
-
         actual = self.transformer.transform_dataset(dataset)
         print(json.dumps(actual, indent=1))
         self.assertEqual(expected, actual)
