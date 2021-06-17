f15ea19dee3a0f3b77bab302c10584e7aec81eb6
vak <vak@macbookpro.local>
2021-06-17 16:05:49 +0200

    Minor refactoring.

--- datastore_builder/common/log_config.py
+++ datastore_builder/common/log_config.py
@@ -1,12 +1,13 @@
 import logging
 
+LOG_FILE_FOR_IMPORT_PIPELINE = "/Users/vak/temp/dataset_import.log"
 
 def log_setup_for_import_pipeline():
     log_format = '%(asctime)s - %(runId)s - %(name)s - %(levelname)s  - %(message)s'
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(logging.Formatter(log_format))
     stream_handler.setLevel(logging.INFO)
-    logging.basicConfig(filename='/Users/vak/temp/dataset_import.log', format=log_format, level=logging.DEBUG)
+    logging.basicConfig(filename=LOG_FILE_FOR_IMPORT_PIPELINE, format=log_format, level=logging.DEBUG)
     logging.getLogger('').addHandler(stream_handler)
 
 
